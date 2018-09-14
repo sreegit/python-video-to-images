44af9adb56195941402da1e7934bb940d61a1cb4
Sreejith Kumar <2662sree@gmail.com>
2018-09-14 21:58:07 +0530

    new utility functions updated. read files from directory

--- file-size.py
+++ file-size.py
@@ -1,6 +1,63 @@
 import os
 
-startCount= 13000
-statinfo = os.stat('./data/frame'+ str(startCount) +'.jpg')
-fileName = 'frame' + str(startCount)
-print (fileName + '.jpg size :' + str(statinfo.st_size))
+def removeFile(filePathName):
+	print ("removing file: " + filePathName)
+	try:
+		os.remove(filePathName)
+	except OSError:
+		pass
+
+def checkFileSize(startCount):
+	filePathName = './data/frame'+ str(startCount) +'.jpg'
+	try:
+		statinfo = os.stat(filePathName)
+		print (filePathName+ ' size :' + str(statinfo.st_size))
+		if statinfo.st_size < 5:
+			print("remove file")
+			removeFile(filePathName)
+			return 1
+		else:
+			print ('valid file')
+			return 0
+	except OSError:
+		print ("Err: file not found")
+		return 0
+		pass
+	
+
+def init():
+	print("Calling file size...")
+	startCount = 13001
+	endCount = 15001
+
+	removeCount = 0
+	for x in range(startCount, endCount+1):
+		count = checkFileSize(x)	
+		removeCount = removeCount + count
+
+	print(removeCount)
+
+##init()
+
+
+
+
+from os import listdir
+from os.path import isfile, join
+from time import sleep
+
+def listFiles():
+	videoPath = '/media/sks/All-Data/Camera-Hik-All/09122018-Sep'
+	onlyfiles = [f for f in listdir(videoPath) if isfile(join(videoPath, f))]
+	print (len(onlyfiles))
+
+	i = 0
+	for x in onlyfiles:
+		print (x)
+		sleep(0.3)
+		i += 1
+		print (i)
+
+listFiles()
+
+
