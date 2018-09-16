f1df2bb5eb912cf01ffd67bff98906252667c2b1
Sreejith Kumar <2662sree@gmail.com>
2018-09-16 12:07:41 +0530

    working file updated, count of image updated

--- video-image.py
+++ video-image.py
@@ -1,44 +1,40 @@
-'''
-Using OpenCV takes a mp4 video and produces a number of images.
-Requirements
-----
-You require OpenCV 3.2 to be installed.
-Run
-----
-Open the main.py and edit the path to the video. Then run:
-$ python main.py
-Which will produce a folder called data with the images. There will be 2000+ images for example.mp4.
-'''
 import cv2
-import numpy as np
 import os
-
-# Playing video from file:
-cap = cv2.VideoCapture('hiv00000.mp4')
+#provide the video number here
+videoNum = 23
+#provide the video path here
+videoPath = '/media/sks/All-Data/Camera-Hik-All/09122018-Sep/'
+####make necessary changes whenver there is a change in the video number (eg. from 9 to 10)
+videoFileName = 'hiv000'+str(videoNum) +'.mp4'
+inputVideo = videoPath+videoFileName
+cap = cv2.VideoCapture(inputVideo)
 
 try:
     if not os.path.exists('data'):
         os.makedirs('data')
 except OSError:
-    print ('Error: Creating directory of data')
-#to improve this
-#sec = 0
-currentFrame = 0
-while(True):
-    #set seconds
-   # cap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    print ("Error: Creating the directory!")
 
-    # Saves image of the current frame in jpg file
-    name = './data/frame' + str(currentFrame) + '.jpg'
-    print ('Creating...' + name)
-    cv2.imwrite(name, frame)
+def getFrame(sec):
+    cap.set(cv2.CAP_PROP_POS_MSEC, sec*1000)
+    hasFrames, frame = cap.read()
+    #if ret:
+    name = './data/v'+str(videoNum)+'_image' + str(sec) + '.jpg'
+    print ('Creating... ' + name)
+    if hasFrames:
+        cv2.imwrite(name, frame)
+    return hasFrames
+sec = 0
+frameRate = 1
 
-    # To stop duplicate images
-    currentFrame += 1
-
-
+success = getFrame(sec)
+while success:
+    sec = sec + frameRate
+    #stopping the execution if the seconds exceeds 600 (max 600 images only)
+    if sec > 600 :
+        break
+    sec = round(sec, 2)
+    success = getFrame(sec)
 
 # When everything done, release the capture
 cap.release()
