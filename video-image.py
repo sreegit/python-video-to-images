bf27f36ba8a7518c9957351eca8df1724e93b2a5
Sreejith Kumar <2662sree@gmail.com>
2018-09-16 13:11:04 +0530

    working config.json and related file commited

--- video-image.py
+++ video-image.py
@@ -1,17 +1,26 @@
 import cv2
 import os
-#provide the video number here
-videoNum = 23
+import json
+#read file config.json
+with open('config.json', 'r') as f:
+    configData = json.load(f)
+
+videoNum = configData["videoNumber"]
 #provide the video path here
-videoPath = '/media/sks/All-Data/Camera-Hik-All/09122018-Sep/'
+videoPath = configData["dirPath"]
+# videoPath = '/media/sks/All-Data/Camera-Hik-All/09122018-Sep/'
+
+dirName = 'data'
+dirVidNum = dirName+str(videoNum)
+
 ####make necessary changes whenver there is a change in the video number (eg. from 9 to 10)
 videoFileName = 'hiv000'+str(videoNum) +'.mp4'
 inputVideo = videoPath+videoFileName
 cap = cv2.VideoCapture(inputVideo)
 
 try:
-    if not os.path.exists('data'):
-        os.makedirs('data')
+    if not os.path.exists( dirVidNum ):
+        os.makedirs( dirVidNum )
 except OSError:
     print ("Error: Creating the directory!")
 
@@ -19,7 +28,7 @@ def getFrame(sec):
     cap.set(cv2.CAP_PROP_POS_MSEC, sec*1000)
     hasFrames, frame = cap.read()
     #if ret:
-    name = './data/v'+str(videoNum)+'_image' + str(sec) + '.jpg'
+    name = './'+ dirVidNum +'/v'+str(videoNum)+'_image' + str(sec) + '.jpg'
     print ('Creating... ' + name)
     if hasFrames:
         cv2.imwrite(name, frame)
